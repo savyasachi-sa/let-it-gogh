95378acfc9effc80ee08ed19222bee1006863832
savyasachi <ssavyasa@dsmlp-login.ucsd.edu>
2019-12-08 19:56:04 -0800

    Add Comments in Experiments and add loss plot in demo

--- neural-style-transfer/StyleTransferTrain.py
+++ neural-style-transfer/StyleTransferTrain.py
@@ -34,7 +34,7 @@ def train(T, style_layers, content_layers, style_weights, content_weights, num_e
 
     n_iter = 0
     show_iter = 50
-    while n_iter <= num_epochs:
+    while n_iter < num_epochs:
         
         def closure():        
             optimizer.zero_grad()
@@ -47,11 +47,47 @@ def train(T, style_layers, content_layers, style_weights, content_weights, num_e
         optimizer.step(closure)
         n_iter += 1
 
-        # print loss
-        if n_iter % show_iter == (show_iter - 1):
+        if n_iter % show_iter == (0):
             print(n_iter, "Completed")
 
     out_img = postProcess(opt_img.cpu().squeeze())
 
     return out_img
 
+## Main Training method
+def trainAndReturnLoss(T, style_layers, content_layers, style_weights, content_weights, num_epochs):
+    weights = style_weights + content_weights
+
+    style_image = T[2].to(device)
+    content_image = T[3].to(device)
+    opt_img = T[4].to(device)
+
+    optimizer = optim.LBFGS([opt_img]);
+    net, loss_layers, loss_fns, targets = StyleTransferNet.init(
+        style_image, content_image, style_layers, content_layers)
+
+    n_iter = [0]
+    show_iter = 50
+    
+    losses = {}
+    
+    while n_iter[0] < num_epochs:
+        
+        def closure():        
+            optimizer.zero_grad()
+            out = net(opt_img, loss_layers)
+            layer_losses = [weights[a] * loss_fns[a](A, targets[a]) for a, A in enumerate(out)]
+            loss = sum(layer_losses)
+            losses[n_iter[0]] = loss
+            loss.backward()
+            return loss
+
+        optimizer.step(closure)
+        n_iter[0] += 1
+        
+        if n_iter[0] % show_iter == (0):
+            print(n_iter[0], "Completed")
+
+    out_img = postProcess(opt_img.cpu().squeeze())
+
+    return out_img, losses
